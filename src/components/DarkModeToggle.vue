2355b41c40bdc4d26d837cc2e3e6382604c569e9
Julian Schäfer <schaefer@synyx.de>
2022-05-13 16:05:41 +0200

    Add darkmode toggle

---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="toggleDark()">
+    <span class="flex items-center gap-2">
+      <MoonIcon v-if="isDark" />
+      <SunIcon v-else />
+      {{ isDark ? "Dark" : "Light" }}
+    </span>
+  </button>
+</template>
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+import MoonIcon from "./icons/MoonIcon.vue";
+import SunIcon from "./icons/SunIcon.vue";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
