119bd04aac8c50c34dac6cb712018c268b52e935
Julian Schäfer <schaefer@synyx.de>
2022-05-13 14:48:39 +0200

    Add prettier and eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), vue(), tailwind()]
+  integrations: [preact(), vue(), tailwind()],
 });
