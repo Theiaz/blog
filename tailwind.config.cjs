4c4bed5511f4b819f5bc16981e8fdd1d0bb48bf9
Julian Schäfer <schaefer@synyx.de>
2022-05-27 14:32:30 +0200

    Set default colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,22 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,ts,vue}"],
   darkMode: "class",
   theme: {
     fontFamily: {
       sans: ["Inter", ...defaultTheme.fontFamily.sans],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "light-primary": "#fff",
+        "light-secondary": "#1d3354ff",
+        "light-accent": "#8c0105ff",
+        "dark-primary": "#1d3354ff",
+        "dark-secondary": "#e9fff9ff",
+        "dark-accent": "#d64045ff",
+      },
+    },
   },
   plugins: [],
 };
