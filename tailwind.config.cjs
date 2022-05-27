dcf790b4e1d02eaf047793947bd282447eb3e4e3
Julian Schäfer <schaefer@synyx.de>
2022-05-27 14:03:53 +0200

    Add custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
