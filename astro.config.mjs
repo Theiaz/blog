c69b1bc826b121cb3eb1c43e68cb8b0c6a1ab710
Julian S <Theiaz@users.noreply.github.com>
2022-06-03 14:11:07 +0000

    Remove unused preact dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
-
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    preact(),
     vue(),
     tailwind({
       // needed, cause we are defining our own @layer directive
