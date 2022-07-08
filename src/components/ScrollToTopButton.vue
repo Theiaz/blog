4ff05f61eb8ae1cc5fe85dcdd5e6689d8d0e91ec
Julian Schäfer <schaefer@synyx.de>
2022-07-08 14:15:59 +0200

    Add scroll to top button

---
+++ src/components/ScrollToTopButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <Transition name="fade">
+    <button
+      v-show="isScrolled"
+      class="fixed bottom-8 right-8 border-none rounded-full p-2 bg-light-secondary dark:bg-dark-secondary"
+      @click="scrollToTop"
+    >
+      <ChevronUpIcon
+        class="text-light-primary dark:text-dark-primary"
+      ></ChevronUpIcon>
+    </button>
+  </Transition>
+</template>
+
+<script setup>
+import ChevronUpIcon from "./icons/ChevronUpIcon.vue";
+import { ref, onMounted, onUnmounted } from "vue";
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+let isScrolled = ref(false);
+function onScroll() {
+  isScrolled.value = true;
+
+  if (window.scrollY == 0) {
+    isScrolled.value = false;
+  }
+}
+onMounted(() => {
+  window.addEventListener("scroll", onScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", onScroll);
+});
+</script>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
