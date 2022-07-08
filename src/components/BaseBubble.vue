bf86b54678bfed94b3164c1c7ba0dd8795bbecc7
Julian Schäfer <schaefer@synyx.de>
2022-07-08 11:17:49 +0200

    WIP Add bubble background

---
+++ src/components/BaseBubble.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="circle animateBubble absolute -z-10 h-20 w-20 bg-gradient-to-br from-light-primary to-light-secondary right-20"
+  ></div>
+</template>
+
+<script setup>
+const props = defineProps({
+  xPos: {
+    type: Number,
+    default: 0,
+  },
+});
+</script>
+
+<style scoped>
+@keyframes animateBubble {
+  0% {
+    top: 100%;
+  }
+  100% {
+    top: -10%;
+  }
+}
+
+@keyframes sideWays {
+  0% {
+    margin-left: 0px;
+  }
+  100% {
+    margin-left: 50px;
+  }
+}
+
+.circle {
+  clip-path: circle(50%);
+  animation: animateBubble 5s linear infinite,
+    sideWays 2s ease-in-out infinite alternate;
+}
+</style>
---
+++ src/components/BubbleList.vue
@@ -0,0 +1,7 @@
+<template>
+  <BaseBubble v-for="n in 10" :key="n" :x-pos="250" />
+</template>
+
+<script setup lang="ts">
+import BaseBubble from "./BaseBubble.vue";
+</script>
