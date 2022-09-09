b7fddc38a893abd16dcc2c25de15bf8973f2c65f
Julian Schäfer <schaefer@synyx.de>
2022-09-09 14:43:20 +0200

    Use mdx files instead of md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
       // needed, cause we are defining our own @layer directive
       config: { applyBaseStyles: false },
     }),
+    mdx(),
   ],
 });
