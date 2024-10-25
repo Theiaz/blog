7731af26e953c5a3ceac0e58ea574095a101c747
Julian Schaefer <schaefer@synyx.de>
2024-10-25 07:38:56 +0200

    chore: format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,15 @@
 import eslint from "@eslint/js";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
 import prettierConfig from "eslint-config-prettier";
 import pluginAstro from "eslint-plugin-astro";
 import pluginVue from "eslint-plugin-vue";
 import tseslint from "typescript-eslint";
-import vueParser from "vue-eslint-parser";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/recommended"],
+  ...vueTsEslintConfig(),
   ...pluginAstro.configs.recommended,
-  prettierConfig,
-  {
-    files: ["**/*.ts", "**/*.astro", "**/*.vue"],
-    rules: {},
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        parser: {
-          ts: tseslint.parser,
-        },
-      },
-    },
-  }
+  prettierConfig
 );
