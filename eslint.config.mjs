2ae2170b0cb8ac3cc9fa40e5e1bb66003c771a43
Julian Schaefer <schaefer@synyx.de>
2024-10-25 12:32:02 +0200

    chore: format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,5 +11,5 @@ export default tseslint.config(
   ...pluginVue.configs["flat/recommended"],
   ...vueTsEslintConfig(),
   ...pluginAstro.configs.recommended,
-  prettierConfig
+  prettierConfig,
 );
